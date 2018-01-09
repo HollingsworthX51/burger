b6c865f89cf1c43465f77f0f65285a34bb5fc675
hworthX51@hotmail.com <you@example.com>
2018-01-09 13:20:30 -0500

    added routes

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
-
 var exphbs = require("express-handlebars");
 
-var port = process.env.PORT || 3000;
 var app = express();
+app.use(express.static(__dirname + '/public'));
+//static folder
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine','handlebars');
+//references routes.js
+var routes = require('./controllers/routes.js');
+app.use('/',routes);
+
+var port = 3000;
+app.listen(port);
