1d16d073012ff9d28ee4c1b2add19a95638bfb4f
hworthX51@hotmail.com <you@example.com>
2018-01-09 16:17:02 -0500

    working on handlebars - fixes

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+--populate table with at least three burgers
+
+INSERT INTO burgers (burger_name)
+VALUES ("Oregano Buger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Kingdom Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Big Mac Burger");
